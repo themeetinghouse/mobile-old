c5f7fdf27d713e05eb6ab357522d2ba96265b9c2
George Bell <george.bell@themeetinghouse.com>
2020-02-04 03:07:18 -0500

    Revert "Group editing"
    This reverts commit 83cad78403b7fcc2787a01cf00468e68b1ed7aaf.

--- screens/HomeScreen/index.tsx
+++ screens/HomeScreen/index.tsx
@@ -15,7 +15,7 @@ import { createAppContainer } from "react-navigation";
 import awsconfig from '../../src/aws-exports';
 import { NavigationScreenProp } from 'react-navigation';
 
-const ConversationScreen = lazy(() => import('../ConversationScreen/ConversationScreen'));
+const ConversationScreen  = lazy(() => import('../ConversationScreen/ConversationScreen'));
 const OrganizationsScreen = lazy(() => import('../OrganizationsScreen/OrganizationsScreen'));
 const OrganizationScreen = lazy(() => import('../OrganizationScreen/OrganizationScreen'));
 const CourseScreen = lazy(() => import('../CourseScreen/CourseScreen'));
@@ -55,12 +55,7 @@ const MainAppRouter = createStackNavigator({
 },
   {
     initialRouteName: 'HomeScreen',
-    headerMode: 'none',
-    mode:'modal',
-    defaultNavigationOptions: {
-      gestureEnabled: false,
-      cardOverlayEnabled:false
-    }
+    headerMode:'none'
   })
 
 const HomeScreenRouter = createDrawerNavigator(
